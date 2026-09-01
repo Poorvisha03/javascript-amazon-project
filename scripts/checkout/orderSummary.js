63fd90e6769a6271e0374a05c9fc359bbef646fd
Poorvisha03 <poorvisha252005@gmail.com>
2026-09-01 12:06:41 +0530

    made the payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import { deliveryOptions,getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
 export function renderOrderSummary(){
@@ -132,6 +133,7 @@ export function renderOrderSummary(){
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
             calculateCartQuantity('.js-item-quantity',true); 
+            renderPaymentSummary();
         });
         
     });
@@ -161,6 +163,7 @@ export function renderOrderSummary(){
             if(quantityLabel){
                 quantityLabel.innerHTML = itemQuantity;
             }
+            renderPaymentSummary();
         });
     });
 
@@ -172,6 +175,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId} = element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     }); 
 }
