712b0dc93b0e773170608102e5388a26911b5e8f
Poorvisha03 <poorvisha252005@gmail.com>
2026-09-15 16:10:33 +0530

    load products  in beforeAll hook

--- data/products.js
+++ data/products.js
@@ -70,7 +70,10 @@ export function loadProducts(fun){
   });
 
   console.log('load products')
-  fun();
+  
+  if (typeof fun === 'function'){
+    fun();
+  }
   });
 
   xhr.open('GET','https://supersimplebackend.dev/products');
